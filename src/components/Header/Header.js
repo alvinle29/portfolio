dddae38ac96cd60107c8f9c1f44352e54915479a
alvinle29 <bach938@gmail.com>
2022-10-27 19:26:13 +0300

    finish navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,43 @@
 import React from 'react'
+import {FaBars} from 'react-icons/fa'
+
+import styles from "./Header.module.css"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <nav className={styles.nav}>
+        <span className={styles.nav__logo}>
+          Bach Le
+        </span>
+        <label className={styles.nav__toggle} for="check">
+          <FaBars className={styles.bar_icon}/>
+        </label>
+
+        <ul className={styles.nav__list}>
+          <li className={styles.nav__item}>
+            <a href="#home" className="nav__link">
+              Home
+            </a>
+          </li>
+          <li className={styles.nav__item}>
+            <a href="#about" className="nav__link">
+              About
+            </a>
+          </li>
+          <li className={styles.nav__item}>
+            <a href="#projects" className="nav__link">
+              Projects
+            </a>
+          </li>
+          <li className={styles.nav__item}>
+            <a href="#contact" className="nav__link">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
